4ca62a16964812144e610ac63d950dffbef58ec6
gadrian99 <adriangarcia1999@yahoo.com>
2020-09-22 23:40:00 -0400

    table country flag display

--- src/Table.js
+++ src/Table.js
@@ -3,16 +3,18 @@ import numeral from 'numeral'
 import './Table.css'
 
 function Table({ countries }) {
+    console.log(countries)
     return (
         <div className="table">
-            {countries.map(({country, cases}) => (
+            {countries.map(({country, cases, countryInfo}) => (
                 <tr>
+                    <td><img src={countryInfo.flag} className="table__img" ></img></td>
                     <td>{country}</td>
                     <td>
                         <strong>{numeral(cases).format("0,0")}</strong>
                     </td>
                 </tr>
-            ))}            
+            ))}
         </div>
     )
 }
