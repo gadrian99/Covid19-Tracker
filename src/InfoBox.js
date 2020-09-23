a7c0d15ea745ba3f72ee4a2b20050c9f5794427f
gadrian99 <adriangarcia1999@yahoo.com>
2020-09-22 22:45:43 -0400

    infoBox data display format

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -4,15 +4,15 @@ import './InfoBox.css'
 
 function InfoBox({ title, cases, total, active, isRed, ...props }) {
     return (
-        <Card 
-            onClick={props.onClick} 
+        <Card
+            onClick={props.onClick}
             className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"}`}
         >
             <CardContent>
                 <Typography className="infoBox__title" color="textSecondary">
                     {title}
                 </Typography>
-                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{cases}</h2>
+                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{cases} <span> / today</span></h2>
                 <Typography className="infoBox__total" color="textSecondary">
                     {total} Total
                 </Typography>
